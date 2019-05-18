ea20429639b7d2c5330d6448b578408dd4189c9e
Joseph Thomaschaske <joseph@Macbook-pro.local>
2019-05-18 12:22:57 -0500

    added chunking

--- index.js
+++ index.js
@@ -5,14 +5,33 @@ const { parse } = require('json2csv');
 
 const getCategoryLinks = async () => {
     const main = await fetch('https://www.foodnetwork.com/recipes/recipes-a-z');
-    const body = await main.text();
-    const document = new JSDOM(body).window.document;
-    const linkElements = document.getElementsByClassName('o-IndexPagination__a-Button ');
-    let links = [];
-    for (link of linkElements) {
-        links.push('https:' + link.getAttribute("href"));
-    }
-    return links;
+    return [
+        //'https://www.foodnetwork.com/recipes/recipes-a-z/123',
+        //'https://www.foodnetwork.com/recipes/recipes-a-z/a',
+        //'https://www.foodnetwork.com/recipes/recipes-a-z/b',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/c',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/d',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/e',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/f',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/g',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/h',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/i',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/j',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/k',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/l',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/m',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/n',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/o',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/p',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/q',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/r',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/s',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/t',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/u',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/v',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/w',
+        'https://www.foodnetwork.com/recipes/recipes-a-z/xyz'
+    ]
 };
 
 const getRecipeLinks = async (link) => {
@@ -34,55 +53,73 @@ const getRecipeLinks = async (link) => {
                 recipes.push('https:' + element.firstChild.getAttribute('href'));
             }
         }
-        if (recipes.length > 100) {
-            return recipes;
-        }
     }
 
     return recipes;
 };
 
-const getRecipes = async (links) => {
+const pauseAndWait = (maxTime, minTime) => {
+    return new Promise((acc, rej) => {
+        const randomTime = Math.random() * (maxTime - minTime) + minTime;
+        console.log('waiting ' + (randomTime/1000) + 's');
+        setTimeout(() => {
+            console.log('')
+            acc();
+        }, randomTime);
+    });
+}
+
+const getRecipes = async (links, category) => {
     let recipes = [];
     let i = 1;
     let total = links.length;
     for (link of links) {
-        //add random pauses here to be a "nice" bot
-        console.log('fetching: ' + link + '\n' + Math.round(((i/total) * 100)) + '%');
-        const main = await fetch(link);
-        const body = await main.text();
-        const document = new JSDOM(body).window.document;
-        const rawTitle = document.getElementsByClassName('o-AssetTitle')[0];
-        const rawIngredients = document.getElementsByClassName('o-Ingredients__a-Ingredient');
-        const rawDirections = document.getElementsByClassName('o-Method__m-Step');
-        let rawImage = document.getElementsByClassName('o-RecipeLead')[0].getElementsByClassName('m-MediaBlock__a-Image a-Image')[0];
-        if (rawImage) {
-            rawImage = 'https:' + rawImage.getAttribute('src');
-        }
-        let ingredients = [];
-        let directions = [];
+        try {
+            //add random pauses here to be a "nice" bot
+            //await pauseAndWait(5000, 1000);
+            console.log('fetching: ' + link + '\n' + Math.round(((i/total) * 100)) + '%');
+            const main = await fetch(link);
+            const body = await main.text();
+            const document = new JSDOM(body).window.document;
+            const rawTitle = document.getElementsByClassName('o-AssetTitle')[0];
+            const rawIngredients = document.getElementsByClassName('o-Ingredients__a-Ingredient');
+            const rawDirections = document.getElementsByClassName('o-Method__m-Step');
+            let rawImage = document.getElementsByClassName('o-RecipeLead')[0].getElementsByClassName('m-MediaBlock__a-Image a-Image')[0];
+            if (rawImage) {
+                rawImage = 'https:' + rawImage.getAttribute('src');
+            }
+            let ingredients = [];
+            let directions = [];
 
-        for (ingredient of rawIngredients) {
-            ingredients.push(ingredient.textContent);
-        }
-        for (direction of rawDirections) {
-            directions.push(direction.textContent);
-        }
+            for (ingredient of rawIngredients) {
+                ingredients.push(ingredient.textContent);
+            }
+            for (direction of rawDirections) {
+                directions.push(direction.textContent);
+            }
 
-        let recipe = {
-            name : rawTitle.textContent,
-            ingredients : ingredients,
-            directions: directions,
-            source: link,
-            imgSrc: rawImage
-        };
-        recipes.push(recipe);
-        i += 1;
+            let recipe = {
+                name : rawTitle.textContent,
+                ingredients : ingredients,
+                directions: directions,
+                source: link,
+                imgSrc: rawImage
+            };
+            recipes.push(recipe);
+            if (i % 20 == 0) {
+                console.log('converting recipes to json');
+                await createCSVs(recipes, category, Math.round(i/20));
+                recipes = [];
+            }
+            i += 1;
+        } catch(e) {
+            console.log('error parsing: ' + link);
+        }
     }
-    return recipes;
+    await createCSVs(recipes, category, Math.round(i/20));
 }
 
-const createCSVs = async (recipeJson, category) => {
+const createCSVs = async (recipeJson, category, chunk) => {
     let recipes = [];
     let directions = [];
     let ingredients = [];
@@ -90,7 +127,8 @@ const createCSVs = async (recipeJson, category) => {
         recipes.push({
             id: index,
             name: recipe.name.replace(/(?:\r\n|\r|\n)/g, '').trim(),
-            source: recipe.source.replace(/(?:\r\n|\r|\n)/g, '').trim()
+            source: recipe.source.replace(/(?:\r\n|\r|\n)/g, '').trim(),
+            imgSrc: recipe.imgSrc ? recipe.imgSrc.replace(/(?:\r\n|\r|\n)/g, '').trim() : ''
         });
 
         recipe.ingredients.forEach((ingredient) => {
@@ -117,10 +155,12 @@ const createCSVs = async (recipeJson, category) => {
     const directionCsv = parse(directions, {fields});
 
     //make file here for category links
-    fs.mkdirSync('./recipes/' + category);
-    fs.writeFileSync('./recipes/' + category + '/recipes.csv', recipeCsv, 'utf-8');
-    fs.writeFileSync('./recipes/' + category + '/ingredients.csv', ingredientCsv, 'utf-8');
-    fs.writeFileSync('./recipes/' + category + '/directions.csv', directionCsv, 'utf-8');
+    if (!fs.existsSync('./recipes/' + category)) {
+        fs.mkdirSync('./recipes/' + category);
+    }
+    fs.writeFileSync('./recipes/' + category + '/recipes' + '-' + chunk +'.csv', recipeCsv, 'utf-8');
+    fs.writeFileSync('./recipes/' + category + '/ingredients' + '-' + chunk +'.csv', ingredientCsv, 'utf-8');
+    fs.writeFileSync('./recipes/' + category + '/directions' + '-' + chunk +'.csv', directionCsv, 'utf-8');
 }
 
 (async () => {
@@ -128,13 +168,13 @@ const createCSVs = async (recipeJson, category) => {
     const links = await getCategoryLinks();
     for (link of links) {
         const savedLink = link;
+        const urlSections = savedLink.split('/');
+        const category = urlSections[urlSections.length - 1];
+
         console.log('getting recipe links');
         const recipeLinks = await getRecipeLinks(link);
         console.log('getting recipes');
-        const recipes = await getRecipes(recipeLinks);
-        console.log('converting recipes to json');
-        const urlSections = savedLink.split('/');
-        const category = urlSections[urlSections.length - 1];
-        await createCSVs(recipes, category);
+        await getRecipes(recipeLinks, category);
+        await pauseAndWait(900000, 900000);
     }
 })();
